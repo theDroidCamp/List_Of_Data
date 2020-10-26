c9998ee72eee0e622b5a1f5b67cc6fa573c6876a
Noel-elon <noelnwaelugo@gmail.com>
2020-10-26 08:01:11 +0100

    sample code

---
+++ app/src/main/java/com/noelon/listofdata/Constants.kt
@@ -0,0 +1,31 @@
+package com.noelon.listofdata
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+
+data class FavouriteBook(
+    val name: String,
+    val rating: Int,
+    val author: String
+)
+
+
+val favouriteBookList = arrayListOf<FavouriteBook>(
+    FavouriteBook("HarryPotter", 5, "JK rowling"),
+    FavouriteBook("Life of pi", 3, "Yann Martel"),
+    FavouriteBook("Davinci code", 4, "Dan brown"),
+    FavouriteBook("Memoirs of a geisha", 4, "Arthur Golden"),
+    FavouriteBook("A man called ove", 5, "Fredrick Backman"),
+    FavouriteBook("Twilight", 1, "Stephenie Meyer"),
+    FavouriteBook("The book thief", 2, "Markus Zusak"),
+    FavouriteBook("Gone girl", 2, "Gillian Flynn"),
+    FavouriteBook("The help", 1, "Kathryn Stockett"),
+    FavouriteBook("Fight club", 4, "Chuck palahniuk")
+)
+
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+}
--- app/src/main/java/com/noelon/listofdata/MainActivity.kt
+++ app/src/main/java/com/noelon/listofdata/MainActivity.kt
@@ -2,10 +2,23 @@ package com.noelon.listofdata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: NoelAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        adapter = NoelAdapter(favouriteBookList)
+        linearLayoutManager = LinearLayoutManager(this)
+
+        recycler_view.adapter = adapter
+        recycler_view.layoutManager = linearLayoutManager
+        
     }
 }
